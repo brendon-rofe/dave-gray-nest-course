73c75a3b64cdd5df53b1b28b2b0abe25b3abaabe
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-09-05 11:19:36 +0200

    feat: Create initial get routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private userService: UsersService) {}
 
-  
+  @Get()
+  findAll() {
+    return []
+  }
+
+  @Get(":id")
+  findById(@Param("id") id: string) {
+    return { id }
+  }
 
 }
